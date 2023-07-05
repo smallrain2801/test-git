4438fea37ecceb5197d2bd86de37ea221d370c1c
cxy <chenxiaoyu@g-soft.com.cn>
2023-07-05 14:05:55 +0800

    666

---
+++ src/main/java/com/zktx/testgit/test/Person2.java
@@ -0,0 +1,22 @@
+package com.zktx.testgit.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author： cxy
+ * @Date： 2022/5/4
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person2 {
+    Integer id;
+
+    String name;
+
+    public static void main(String[] args) {
+        System.out.println("sss\t\nwww");
+    }
+}
